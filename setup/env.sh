3a9130e3fe8afad6bf9d084da6bb235f1e06378d
imssyang <imssyang@gmail.com>
2021-10-04 09:55:55 +0800

    update php

--- setup/env.sh
+++ setup/env.sh
@@ -2,8 +2,7 @@
 
 export PHP_DISABLE_ENV=yes
 export PHPRC=/opt/php/lib
-export PHP_INI_SCAN_DIR=/opt/php/lib/php.d
+export PHP_INI_SCAN_DIR=/opt/php/conf
 eval "optbin -s /opt/php/bin"
-eval "optbin -s /opt/php/sbin"
 
 
